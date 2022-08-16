83b5b3ed54b129007ea8adb719ac62d88c50ebee
Izac Peterson <ipeterson@bhqut.local>
2022-08-16 12:56:40 -0600

    setup vue

---
+++ app/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <h1>Header</h1>
+  </header>
+
+  <RouterView />
+</template>
+
+<script>
+async function fetchData() {
+  const res = await fetch('/api/test')
+  const data = await res.json()
+  console.log(data);
+}
+
+fetchData();
+</script>
