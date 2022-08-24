c02939bb0468186c1f44c2d28922169de4c5c981
izacpeterson <izacpeterson@gmail.com>
2022-08-23 18:54:32 -0600

    ignore fix

--- app/src/components/PageHeader.vue
+++ app/src/components/PageHeader.vue
@@ -1,5 +1,6 @@
 <template>
   <header class="p-5 bg-primary dark:bg-zinc-800 text-white flex items-center w-full justify-evenly shadow">
+    <img width="50" src="../assets/bug.svg" alt="" />
     <h1 class="text-2xl">Izac's Bug Tracker</h1>
     <ul class="flex-grow flex justify-center">
       <li class="m-2"><router-link to="/">Home</router-link></li>
