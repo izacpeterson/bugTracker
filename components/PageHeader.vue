f055eec80c1c3d2d8afd790e70baf51274a7ad2c
Izac Peterson <ipeterson@bhqut.local>
2022-11-30 12:29:35 -0700

    blank

--- components/PageHeader.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <header class="navbar bg-neutral flex justify-between">
-    <router-link to="/" class="text-xl"> Bug Tracker </router-link>
-    <router-link to="/about" class="link">About</router-link>
-  </header>
-</template>
--- layouts/default.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <PageHeader></PageHeader>
-    <main class="p-2">
-      <Nuxt />
-    </main>
-  </div>
-</template>
